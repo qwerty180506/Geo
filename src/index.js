189cc02fb4639d9f14e89145bf49ac0a3ac412df
qwerty180506 <thivvisiva@gmail.com>
2026-05-28 18:43:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,11 @@ import { runJioTV } from "./jiotv.js";
 import { runFancode } from "./fancode.js";
 
 export default {
-  // ---------------- CRON JOB ----------------
-  async scheduled(event, env, ctx) {
-    try {
-      await Promise.all([
-        runJioTV(env),
-        runFancode(env),
-      ]);
-
-      console.log(
-        "Both playlists updated successfully"
-      );
-    } catch (e) {
-      console.log("Cron Error:", e.toString());
-    }
-  },
-
   // ---------------- MANUAL URL TRIGGER ----------------
   async fetch(request, env) {
     try {
-      await Promise.all([
-        runJioTV(env),
-        runFancode(env),
-      ]);
+      await runJioTV(env);
+      await runFancode(env);
 
       return new Response(
         "Both playlists updated successfully"
