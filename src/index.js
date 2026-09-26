c0f436335f226bb180f10b38eb1bfdd5f8a98a42
qwerty180506 <thivvisiva@gmail.com>
2026-09-26 13:14:57 +0530

    Remove Hotstar integration from index.js
    Removed the import and usage of runHotstar function.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import { runJioTV } from "./jiotv.js";
 import { runJioTV2 } from "./jiotv2.js";
 import { runFancode } from "./fancode.js";
-import { runHotstar } from "./Hotstar.js";
 import { runMerge } from "./playlist.js";
 
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-
-    if (
-      url.pathname === "/playlist" ||
-      url.pathname === "/stream" ||
-      url.pathname === "/proxy"
-    ) {
-      return await runHotstar(request);
-    }
-
     try {
       await Promise.all([
         runFancode(env),
