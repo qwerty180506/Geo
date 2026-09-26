dd12bfc8acad704c9c24e687504dc5b48a7a69c9
qwerty180506 <thivvisiva@gmail.com>
2026-09-26 10:30:31 +0530

    Refactor Hotstar import and update fetch logic

--- src/index.js
+++ src/index.js
@@ -1,31 +1,35 @@
 import { runJioTV } from "./jiotv.js";
 import { runJioTV2 } from "./jiotv2.js";
 import { runFancode } from "./fancode.js";
-import { runChannelFilter } from "./Hotstar.js";
+import { runHotstar } from "./Hotstar.js";
 import { runMerge } from "./playlist.js";
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (
+      url.pathname === "/playlist" ||
+      url.pathname === "/stream" ||
+      url.pathname === "/proxy"
+    ) {
+      return await runHotstar(request);
+    }
+
     try {
       await Promise.all([
         runFancode(env),
-        //runJioTV(env),
-        runJioTV2(env)
-        //runChannelFilter(env)
+        // runJioTV(env),
+        runJioTV2(env),
       ]);
 
       await runMerge(env);
 
-      return new Response(
-        "All playlists updated successfully"
-      );
+      return new Response("All playlists updated successfully");
     } catch (e) {
-      return new Response(
-        "Error: " + e.toString(),
-        {
-          status: 500,
-        }
-      );
+      return new Response("Error: " + e.toString(), {
+        status: 500,
+      });
     }
   },
 };
